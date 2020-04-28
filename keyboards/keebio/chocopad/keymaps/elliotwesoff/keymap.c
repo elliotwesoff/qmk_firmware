ba15cad61fb2dad871e1c9d7747b058d921080e6
Elliot Wesoff <ewesoff@gmail.com>
2020-04-28 11:45:56 -0700

    new chocopad keymap yay!

--- keyboards/keebio/chocopad/keymaps/elliotwesoff/keymap.c
+++ keyboards/keebio/chocopad/keymaps/elliotwesoff/keymap.c
@@ -7,22 +7,22 @@
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
   [_BASE] = LAYOUT_ortho_4x4(
-    KC_PGUP,  KC_HOME,  KC_UP,    KC_END , \
-    KC_PGDN,  KC_LEFT,  KC_DOWN,  KC_RGHT, \
-    MO(_FN2), KC_VOLU,  KC_MPLY,  KC_MPRV, \
-    MO(_FN1), KC_VOLD,  KC_MUTE,  KC_MNXT  \
+    KC_P7,  KC_P8, KC_P9, KC_KP_PLUS, \
+    KC_P4, KC_P5, KC_P6, KC_KP_MINUS, \
+    KC_P1, KC_P2, KC_P3, KC_KP_ASTERISK, \
+    KC_DOT, KC_P0, KC_KP_SLASH, TO(_FN1)
   ),
   [_FN1] = LAYOUT_ortho_4x4(
-    KC_ESC,   KC_P7,    KC_P8,    KC_P9,   \
-    KC_TAB,   KC_P4,    KC_P5,    KC_P6,   \
-    KC_ENT,   KC_P1,    KC_P2,    KC_P3,   \
-    _______,  KC_P0,    KC_P0,    KC_DOT   \
+    KC_F1, KC_F2, KC_F3, KC_F4, \
+    KC_F5, KC_F6, KC_F7, KC_F8, \
+    KC_F9, KC_F10, KC_11, KC_12, \
+    TO(_BASE), LCTRL(KC_F8), LSFT(KC_F11), TO(_FN2)
   ),
   [_FN2] = LAYOUT_ortho_4x4(
-    RGB_TOG,  RGB_HUI,  RGB_SAI,  RGB_VAI, \
-    RGB_MOD,  RGB_HUD,  RGB_SAD,  RGB_VAD, \
-    _______,  _______,  _______,  RESET,  \
-    BL_STEP,  _______,  _______,  _______  \
+    KC_G, KC_G, KC_G, KC_G \
+    KC_G, KC_G, KC_G, KC_G \
+    KC_G, KC_G, KC_G, KC_G \
+    TO(_FN1), KC_G, KC_G, KC_G
   )
 
 };
