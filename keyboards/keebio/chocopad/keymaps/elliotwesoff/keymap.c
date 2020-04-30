cd758976276e6e762359666eb4fd8c27bba499e0
Elliot Wesoff <ewesoff@gmail.com>
2020-04-29 17:01:48 -0700

    update chocopad layers.

--- keyboards/keebio/chocopad/keymaps/elliotwesoff/keymap.c
+++ keyboards/keebio/chocopad/keymaps/elliotwesoff/keymap.c
@@ -1,8 +1,8 @@
 #include QMK_KEYBOARD_H
 
 #define _BASE 0
-#define _NUMPAD 1
-#define _FNS 2
+#define _FNS 1
+#define _NUMPAD 2
 #define _GAMING 3
 #define _REAPER 4
 
@@ -17,8 +17,49 @@ enum custom_keycodes {
   LAYER_DOWN,
   UNDO,
   REDO,
-  COD_PING
+  COD_PING,
+  DISCORD_MUTE
 };
+
+const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
+
+  [_BASE] = LAYOUT_ortho_4x4(
+    WIN_SCN_LFT, POP_SCN_DN, POP_SCN_UP, WIN_SCN_RT, \
+    KC_TILDE,    KC_LBRC,    KC_RBRC,    KC_BSLS, \
+    KC_MUTE,     KC_VOLD,    KC_VOLU,    KC_MPLY, \
+    CTL_ALT_DEL, KC_LALT,    KC_TAB,     LAYER_UP \
+  ),
+
+  [_FNS] = LAYOUT_ortho_4x4(
+    KC_F1,      KC_F2,       KC_F3,        KC_F4, \
+    KC_F5,      KC_F6,       KC_F7,        KC_F8, \
+    KC_F9,      KC_F10,      KC_F11,       KC_F12, \
+    LAYER_DOWN, LCTL(KC_F8), LSFT(KC_F11), LAYER_UP \
+  ),
+
+  [_NUMPAD] = LAYOUT_ortho_4x4(
+    KC_P7,      KC_P8, KC_P9,      KC_KP_PLUS, \
+    KC_P4,      KC_P5, KC_P6,      KC_KP_MINUS, \
+    KC_P1,      KC_P2, KC_P3,      KC_DOT, \
+    LAYER_DOWN, KC_P0, KC_NUMLOCK, LAYER_UP \
+  ),
+
+  [_GAMING] = LAYOUT_ortho_4x4(
+    DISCORD_MUTE, _______,     _______,      COD_PING, \
+    _______,      _______,     _______,      _______, \
+    _______,      _______,     _______,      _______, \
+    LAYER_DOWN,   _______,     _______,      LAYER_UP \
+  ),
+
+  [_REAPER] = LAYOUT_ortho_4x4(
+    LCTL(KC_R), KC_SPACE,    KC_HOME,      KC_R, \
+    UNDO,       REDO,        _______,      _______, \
+    _______,    _______,     _______,      _______, \
+    LAYER_DOWN, _______,     _______,      KC_LGUI \
+  )
+
+};
+
 
 uint16_t active_layer = 0;
 uint16_t top_layer = 4;
@@ -125,46 +166,13 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
       if (record->event.pressed) {
         tap_code16(KC_LALT);
       }
+      break;
+    case DISCORD_MUTE:
+      if (record->event.pressed) {
+        tap_code16(KC_EQUAL);
+      }
+      break;
 
   }
   return true;
 }
-
-const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-
-  [_BASE] = LAYOUT_ortho_4x4(
-    WIN_SCN_LFT, POP_SCN_DN, POP_SCN_UP, WIN_SCN_RT, \
-    KC_TILDE,    KC_LBRC,    KC_RBRC,    KC_BSLS, \
-    KC_MUTE,     KC_VOLD,    KC_VOLU,    KC_MPLY, \
-    CTL_ALT_DEL, KC_LALT,    KC_TAB,     LAYER_UP \
-  ),
-
-  [_NUMPAD] = LAYOUT_ortho_4x4(
-    KC_P7,      KC_P8, KC_P9,      KC_KP_PLUS, \
-    KC_P4,      KC_P5, KC_P6,      KC_KP_MINUS, \
-    KC_P1,      KC_P2, KC_P3,      KC_DOT, \
-    LAYER_DOWN, KC_P0, KC_NUMLOCK, LAYER_UP \
-  ),
-
-  [_FNS] = LAYOUT_ortho_4x4(
-    KC_F1,      KC_F2,       KC_F3,        KC_F4, \
-    KC_F5,      KC_F6,       KC_F7,        KC_F8, \
-    KC_F9,      KC_F10,      KC_F11,       KC_F12, \
-    LAYER_DOWN, LCTL(KC_F8), LSFT(KC_F11), LAYER_UP \
-  ),
-
-  [_GAMING] = LAYOUT_ortho_4x4(
-    KC_F1,      KC_F2,       KC_F3,        COD_PING, \
-    KC_F5,      KC_F6,       KC_F7,        KC_F8, \
-    KC_F9,      KC_F10,      KC_F11,       KC_F12, \
-    LAYER_DOWN, LCTL(KC_F8), CTL_ALT_DEL,  LAYER_UP \
-  ),
-
-  [_REAPER] = LAYOUT_ortho_4x4(
-    LCTL(KC_R), KC_SPACE,    KC_HOME,      KC_R, \
-    UNDO,       REDO,        _______,      _______, \
-    KC_F9,      KC_F10,      KC_F11,       KC_F12, \
-    LAYER_DOWN, LCTL(KC_F8), LSFT(KC_F11), KC_LGUI \
-  )
-
-};
