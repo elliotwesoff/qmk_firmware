c7158db96c683907abae4b4ae4d51af1169cfc9a
Elliot Wesoff <elliot.wesoff@gmail.com>
2020-12-26 22:04:14 -0800

    update sinc keymap.

--- keyboards/keebio/sinc/keymaps/elliotwesoff/keymap.c
+++ keyboards/keebio/sinc/keymaps/elliotwesoff/keymap.c
@@ -6,37 +6,50 @@
 // whichever custom keycode you have defined first in this enum. danny help!
 enum custom_keycodes {
   WAT,
-  APOCALYPSE1,
-  APOCALYPSE2,
-  APOCALYPSE3,
-  PI1,
-  PI4,
-  PI3,
-  OP1,
-  OP2,
-  OP3
+  M1,
+  M2,
+  M3,
+  M4,
+  M5,
+  M6,
+  M7,
+  M8,
+  M9,
+  M10
 };
 
 enum combo_events {
+  PI_COMBO,
   ONEPASS_COMBO,
-  APOCALYPSE_COMBO,
-  PI_COMBO
+  APOCALYPSE_COMBO
 };
 
-const uint16_t PROGMEM onepass_combo[] = {OP1, OP2, OP3, COMBO_END};
-const uint16_t PROGMEM apocalypse_combo[] = {APOCALYPSE1, APOCALYPSE2, APOCALYPSE3, COMBO_END};
-const uint16_t PROGMEM pi_combo[] = {PI3, PI1, PI4, COMBO_END};
+const uint16_t PROGMEM onepass_combo[] = {M1, M8, M10, COMBO_END};
+const uint16_t PROGMEM apocalypse_combo[] = {M5, M6, M10, COMBO_END};
+const uint16_t PROGMEM pi_combo[] = {M1, M3, M4, COMBO_END};
 
 combo_t key_combos[COMBO_COUNT] = {
-  [ONEPASS_COMBO] = COMBO_ACTION(onepass_combo),
+  [PI_COMBO] = COMBO_ACTION(pi_combo),
   [APOCALYPSE_COMBO] = COMBO_ACTION(apocalypse_combo),
-  [PI_COMBO] = COMBO_ACTION(pi_combo)
+  [ONEPASS_COMBO] = COMBO_ACTION(onepass_combo)
 };
 
 void process_combo_event(uint16_t combo_index, bool pressed) {
   switch(combo_index) {
     // ******************************************************
     // for the love of god, do not commit from here...
+    case ONEPASS_COMBO:
+      if (pressed) {
+      }
+      break;
+    case APOCALYPSE_COMBO:
+      if (pressed) {
+      }
+      break;
+    case PI_COMBO:
+      if (pressed) {
+      }
+      break;
     // all the way down to here.
     // i mean it.
     // please don't.
@@ -47,12 +60,12 @@ void process_combo_event(uint16_t combo_index, bool pressed) {
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_80_iso_with_macro(
-    KC_MUTE,                KC_ESC,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
-    PI1,        APOCALYPSE1,KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______,  KC_BSPC, KC_HOME,
-    PI3,        PI4,        KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_END,
-    APOCALYPSE2,APOCALYPSE3,KC_CAPS,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,  KC_INS,
-    OP1,        OP2,        KC_LSFT,    MO(1),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
-    KC_F9,      OP3,        KC_LCTL,    KC_LALT,  KC_LGUI, KC_LALT, _______, KC_LGUI,          _______, KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
+    KC_MUTE,      KC_ESC,     KC_F1,    KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,  KC_DEL,  KC_INS,
+    M1,       M2, KC_GRV,     KC_1,     KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______,  KC_BSPC, KC_HOME,
+    M3,       M4, KC_TAB,     KC_Q,     KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_END,
+    M5,       M6, KC_CAPS,    KC_A,     KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT, KC_BSLS, KC_ENT,  KC_INS,
+    M7,       M8, KC_LSFT,    MO(1),    KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT, KC_UP,   KC_PGDN,
+    M9,       M10,KC_LCTL,    KC_LALT,  KC_LGUI, KC_LALT, _______, KC_LGUI,          _______, KC_SPC,  KC_RALT, KC_RGUI, KC_RCTL, KC_LEFT, KC_DOWN, KC_RGHT
   ),
   [1] = LAYOUT_80_iso_with_macro(
     _______,          _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,
@@ -66,6 +79,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
 
 bool process_record_user(uint16_t keycode, keyrecord_t *record) {
   switch (keycode) {
+    // on the physical keyboard, the "code" key
+    // doesn't actually register when you program
+    // a value to it on the keymap. instead, it
+    // fires the first appearing custom keycode
+    // in the custom_keycodes enum definition. WAT
     case WAT:
       if (record->event.pressed) {
         register_code16(KC_LCTL);
