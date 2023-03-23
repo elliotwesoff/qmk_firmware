7eb287b2fd01c7d863a8562822c57badd85d6bdc
Elliot Wesoff <ewesoff@gmail.com>
2023-03-22 21:07:10 -0700

    update sinc rev3 encoder directions and other km values.

--- keyboards/keebio/sinc/rev3/config.h
+++ keyboards/keebio/sinc/rev3/config.h
@@ -16,6 +16,8 @@ along with this program.  If not, see <http://www.gnu.org/licenses/>.
 
 #pragma once
 
+#include "secrets.h"
+
 #define SERIAL_USART_FULL_DUPLEX
 #define SERIAL_USART_TX_PIN GP0
 #define SERIAL_USART_RX_PIN GP1
--- keyboards/keebio/sinc/sinc.c
+++ keyboards/keebio/sinc/sinc.c
@@ -33,15 +33,15 @@ bool encoder_update_kb(uint8_t index, bool clockwise) {
     if (!encoder_update_user(index, clockwise)) { return false; }
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGDN);
-        } else {
             tap_code(KC_PGUP);
+        } else {
+            tap_code(KC_PGDN);
         }
     } else if (index == 1) {
         if (clockwise) {
-            tap_code(KC_VOLU);
-        } else {
             tap_code(KC_VOLD);
+        } else {
+            tap_code(KC_VOLU);
         }
     }
     return true;
