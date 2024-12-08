78e45d4735a0dac341c608f2cfd7d136af443182
Elliot Wesoff <ewesoff@gmail.com>
2024-12-07 18:12:41 -0800

    small keymap updates

--- keyboards/keebio/sinc/keymaps/elliot/keymap.c
+++ keyboards/keebio/sinc/keymaps/elliot/keymap.c
@@ -1,12 +1,7 @@
 #include <stdbool.h>
 #include QMK_KEYBOARD_H
+#include "secrets.h"
 
-/*
- * note to self:
- * if the keeb won't show up in thunar as a RPI disk drive,
- * restart your computer. if it still doesn't show up, then
- * all hope is lost.
- */
 
 enum custom_keycodes {
   M1, M2, M3, M4, M5, M6, M7, M8, M9, M10
@@ -16,7 +11,7 @@ bool capsLock = false;
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [0] = LAYOUT_80_with_macro(
-    XXXXXXX,                          KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           LGUI(KC_Z),   KC_MUTE,
+    XXXXXXX,                          KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           LGUI(KC_SPC), KC_MUTE,
     LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC,      KC_HOME,
     LGUI(KC_7), LGUI(KC_Z),           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,      KC_END,
     LGUI(KC_5), LGUI(KC_6),           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,       KC_BRIU,
@@ -89,9 +84,9 @@ bool rgb_matrix_indicators_advanced_user(uint8_t led_min, uint8_t led_max) {
 bool encoder_update_user(uint8_t index, bool clockwise) {
     if (index == 0) {
         if (clockwise) {
-            tap_code(KC_PGUP);
+            tap_code(layer_state_is(2) ? KC_LEFT : KC_PGUP);
         } else {
-            tap_code(KC_PGDN);
+            tap_code(layer_state_is(2) ? KC_RIGHT : KC_PGDN);
         }
     } else if (index == 1) {
         if (clockwise) {
