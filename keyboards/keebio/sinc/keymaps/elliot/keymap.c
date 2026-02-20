64f4d5921ac63e4efc6cbe358283232604fc8800
Elliot Wesoff <ewesoff@gmail.com>
2026-02-20 11:37:51 -0800

    remove bad rgb codes, replace bl up/down with pg up/down

--- keyboards/keebio/sinc/keymaps/elliot/keymap.c
+++ keyboards/keebio/sinc/keymaps/elliot/keymap.c
@@ -13,14 +13,14 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
     XXXXXXX,                          KC_ESC,  KC_F1,   KC_F2,   KC_F3,   KC_F4,   KC_F5,   KC_F6,   KC_F7,   KC_F8,   KC_F9,   KC_F10,  KC_F11,  KC_F12,           LGUI(KC_SPC), KC_MUTE,
     LCTL(LSFT(KC_TAB)), LCTL(KC_TAB), KC_GRV,  KC_1,    KC_2,    KC_3,    KC_4,    KC_5,    KC_6,    KC_7,    KC_8,    KC_9,    KC_0,    KC_MINS, KC_EQL,  _______, KC_BSPC,      KC_HOME,
     LGUI(KC_7), LGUI(KC_Z),           KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,    KC_T,    KC_Y,    KC_U,    KC_I,    KC_O,    KC_P,    KC_LBRC, KC_RBRC,          KC_BSLS,      KC_END,
-    LGUI(KC_5), LGUI(KC_6),           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,       KC_BRIU,
-    LGUI(KC_3), LGUI(KC_4),           KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,        KC_BRID,
+    LGUI(KC_5), LGUI(KC_6),           KC_LCTL, KC_A,    KC_S,    KC_D,    KC_F,    KC_G,    KC_H,    KC_J,    KC_K,    KC_L,    KC_SCLN, KC_QUOT,                   KC_ENT,       KC_PGUP,
+    LGUI(KC_3), LGUI(KC_4),           KC_LSFT,          KC_Z,    KC_X,    KC_C,    KC_V,    KC_B,    KC_N,    KC_M,    KC_COMM, KC_DOT,  KC_SLSH, KC_RSFT,          KC_UP,        KC_PGDN,
     LGUI(KC_1), LGUI(KC_2),           MO(1),   KC_LALT, KC_LGUI, KC_LGUI, _______, KC_LALT,          XXXXXXX, KC_SPC,  KC_RGUI, KC_RALT, KC_RCTL, KC_LEFT,          KC_DOWN,      KC_RGHT
   ),
   [1] = LAYOUT_80_with_macro(
     _______,    QK_BOOT, _______, TG(2),   TG(3),   _______, _______, _______, _______, _______, _______,  _______, _______, _______,          KC_PSCR,   _______,
-    M9, M10,    RGB_TOG, RM_PREV, RM_NEXT, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, KC_DELETE, _______,
-    M7, M8,     RGB_MOD, RGB_VAD, RGB_VAI, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
+    M9, M10,    _______, RM_PREV, RM_NEXT, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, KC_DELETE, _______,
+    M7, M8,     _______, _______, _______, _______, _______, _______, _______, _______, _______, _______,  _______, _______, _______, _______, _______,
     M5, M6,     KC_CAPS, _______, _______, _______, _______, _______, KC_LEFT, KC_DOWN, KC_UP,   KC_RIGHT, _______, _______,          _______, _______,
     M3, M4,     _______,          _______, _______, _______, _______, _______, _______, _______, KC_HOME,  KC_END,  _______, _______, KC_VOLU, _______,
     M1, M2,     _______, TG(2),   TG(3),   _______, _______, KC_TRNS,          _______, _______, _______,  _______, _______, _______, KC_VOLD, _______
